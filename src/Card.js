f94d6c2568d1409329e51309797165941785c8f7
Jesus Barrera <jesus.ba015@gmail.com>
2019-11-10 13:12:25 -0600

    Toggle Card state

--- src/Card.js
+++ src/Card.js
@@ -1,13 +1,35 @@
 import React from 'react';
 
-function Card(props) {
-    return (
-        <div className="card">
-            <div className="card__content">
-                {props.label}
+class Card extends React.Component {
+    constructor(props) {
+        super(props);
+        this.toggle = this.toggle.bind(this);
+        this.state = { active: false };
+    }
+
+    toggle(e) {
+        e.preventDefault();
+        this.setState({ active: ! this.state.active });
+    }
+
+    render() {
+        var className = 'card' + (this.state.active ? ' active' : '');
+
+        return (
+            <div
+                className={className}
+                onClick={this.toggle}
+            >
+                <div className="card__name">
+                    {this.props.name}
+                </div>
+
+                <div className="card__content">
+                    {this.props.value}
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default Card;
--- src/Grid.js
+++ src/Grid.js
@@ -3,12 +3,24 @@ import Card from './Card';
 import './cards.css';
 
 class Grid extends React.Component {
-    render() {
-        var cards = ['0', '1/2', '1', '2', '3', '5', '8', '13', '20', '40', '100', 'Inf', '?', 'Bk'];
+    constructor(props) {
+        super(props);
+        this.cards = ['0', '1/2', '1', '2', '3', '5', '8', '13', '20', '40', '100', 'Inf', '?', 'Bk'];
+        this.state = {
+            voting: false
+        };
+    }
 
+    reset() {
+        // set all cards to unselected
+    }
+
+    render() {
         return (
-            <div className="grid">
-                {cards.map((label, key) => (<Card key={key} label={label} />))}
+            <div className="grid grid-4-cols">
+                {this.cards.map((card, key) => (
+                    <Card key={key} value={card} />
+                ))}
             </div>
         );
     }
